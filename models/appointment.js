cce84c828add7e0b8a765826adbb27c8c34aad79
techbyadi <pssharma.aditya@gmail.com>
2024-09-04 16:00:55 -0700

    add create appointment functionality

---
+++ models/appointment.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const appointmentSchema = new Schema({
+  patient : {
+    type: Schema.Types.ObjectId,
+    ref: 'Profile'
+  }, 
+  doctor : {
+    type: Schema.Types.ObjectId,
+    ref: 'Doctor'
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  time: {
+    type: String,
+    required: true
+  },
+  reason: {
+    type: String,
+    required: true
+  },
+  mode: {
+    type : String,
+    required : true,
+    enum: ['In Person', 'Phone Call'] 
+  }
+},{
+  timestamps: true,
+})
+
+const Appointment = mongoose.model('Appointment', appointmentSchema)
+
+export {Appointment}
